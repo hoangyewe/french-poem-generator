09b3336b112bdd7323a773e6285313fc58c72ba6
hoangyen <hoanghoangyy2408@gmail.com>
2024-09-16 18:21:42 +0700

    i changed my html code and fetch api key

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import Typewriter from 'typewriter-effect/dist/core';
-let poemFormElement = document.getElementById("poem-generator");
-poemFormElement.addEventListener('submit', generatePoem);
-function generatePoem(event) {
-    event.preventDefault();
+import axios from 'axios';
+function displayPoem(response) {
+    console.log("poem generated");
     new Typewriter("#poem", {
-        strings: "La tombe dit à la rose",
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
-        cursor: "",
+        cursor: ""
     })
 }
+function generatePoem(event) {
+    event.preventDefault();
+    let instructionsInput = document.querySelector("#user-instructions")
+    let apiKey = "2009ba20620tfaoecec68f2784fdd0a3";
+    let prompt = `Generate a Frend poem about ${instructionsInput.value}`;
+    let context = "You are a romantic Poem expert and love to write short poem. You are heavily impacted by Victor Hugo. If I was your lover, what would you write to me? Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Sign the poem with SheCodes API. Not include the title ";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}`;
+    axios.get(apiUrl).then(displayPoem);
+    console.log("Generating poem");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+    }
+
+let poemFormElement = document.getElementById("poem-generator");
+poemFormElement.addEventListener('submit', generatePoem);
