65a80e08d0788c992ed91f34455fa05de0484fc6
hoangyen <hoanghoangyy2408@gmail.com>
2024-09-17 08:15:09 +0700

    improve UI

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
     let instructionsInput = document.querySelector("#user-instructions")
     let apiKey = "2009ba20620tfaoecec68f2784fdd0a3";
     let prompt = `Generate a French poem about ${instructionsInput.value}`;
-    let context = "You are a romantic Poem expert and love to write short poem. You are heavily impacted by Victor Hugo. If I was your lover, what would you write to me? Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Sign the poem with SheCodes API. Not include the title or any marks";
+    let context = "You are a romantic Poem expert and love to write short poem. You are heavily impacted by Victor Hugo. If I was your lover, what would you write to me? Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Sign the poem with SheCodes API. Not include the title or any punctuation marks";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     axios.get(apiUrl).then(displayPoem);
     console.log("Generating poem");
@@ -23,3 +23,6 @@ function generatePoem(event) {
 
 let poemFormElement = document.getElementById("poem-generator");
 poemFormElement.addEventListener('submit', generatePoem);
+let poemElement = document.querySelector("#result");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class="blink">Genering a French poem about ${instructionsInput.value}</div>`;
